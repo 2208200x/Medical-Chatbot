404fafce2f4d8478d482828aff99d644e14fa800
Chetna Deshmukh <chetnadeshmukh812@gmail.com>
2025-08-19 20:03:35 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="Medical-Chatbot",
+    version='0.0.1',
+    author ="Chetna Deshmukh",
+    author_email="chetnadeshmukh@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
+
